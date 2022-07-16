441df7b6f99df64d41bf6a4b84453ea79bc71e0c
Silver-Eun <h22sang@naver.com>
2022-07-16 17:39:11 +0900

    values ​​of the Fibonacci sequence

---
+++ algorithm29/algorithm29.cpp
@@ -0,0 +1,19 @@
+﻿#include<stdio.h>
+void main() {
+	int A = 1, B = 1, T, H;
+	T = A + B;
+
+	puts("values ​​of the Fibonacci sequence");
+	printf("%d %d", A, B);
+	
+	do {
+		H = A + B;
+		T = T + H;
+		A = B;
+		B = H;
+
+		printf("%d ", B);
+	} while (H < 34);
+
+	printf(" = %d\n", T);
+}
